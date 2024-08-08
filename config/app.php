ced3add382d2420b70ee46b622d6278d5b521865
Yu Tsou <yutsou@Yus-MacBook-Air.local>
2024-08-08 15:48:24 +0800

    update

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
         /*
          * Package Service Providers...
          */
-        Mews\Captcha\CaptchaServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
